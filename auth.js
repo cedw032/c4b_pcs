476e174ca409e7d62a4507ede3ee390c17544269
Chad Edwards <chad@fergusapp.com>
2019-09-03 04:16:28 +1200

    First production test

--- auth.js
+++ auth.js
@@ -1,10 +1,15 @@
 module.exports = {
 	isEndClient: socket => {
-		console.log('END CLIENT ADDRESS', socket.handshake.address);
+		console.log(socket.handshake.headers.referer);
+		/*return socket.handshake.headers.referer === 'http://localhost:3000/';*/
+		return socket.handshake.headers.referer === 'https://https://cedw032.github.io/c4b_cw/build/';
 	},
 
 	isBusinessClient: socket => {
-		console.log('BUSINESS ADDRESS', socket.handshake.address);
+		console.log(socket.handshake.headers.referer);
+		return socket.handshake.headers.referer === 'https://still-falls-37601.herokuapp.com/';
+		/*return socket.handshake.headers.referer === 'http://localhost:5000/'
+			|| socket.handshake.headers.referer === 'http://localhost:3001/';*/
 	},
 
 	isBusinessLogin: ({username, password}) => {
--- manageRoles.js
+++ manageRoles.js
@@ -1,3 +1,5 @@
+const moment = require('moment');
+
 module.exports = (io, auth) => {
 
 	let sockets = {
@@ -7,29 +9,36 @@ module.exports = (io, auth) => {
 
 	let endClientSocketsAllocated = 0;
 
-	const broadcastEndClients = () => {
-		object.entries(sockets.businessSignedIn).forEach(([username, sockets]) => {
-			sockets.forEach(socket => {
-				socket.emit('endClients', Object.keys(sockets.endClientGuest))
-			});
+	const forEachBusinessSocket = task => {
+		Object.entries(sockets.businessSignedIn).forEach(([username, sockets]) => {
+			sockets.forEach(socket => task(socket));
 		});
+	}
+
+	const broadcastEndClients = () => {
+		const endClients = Object.keys(sockets.endClientGuest);
+		forEachBusinessSocket(socket => socket.emit('endClients', endClients));
 	}
 
 	const broadcastIsBusinessAvailable = () => {
 		Object.entries(sockets.endClientGuest).forEach(([id, socket]) => {
+			console.log('EMITTING RESPONSE YEYE');
+
+			//console.log('bIN', sockets.businessSignedIn);
+
+			const isBusinessAvailable = Object.entries(sockets.businessSignedIn).reduce(
+				(isBusinessAvailable, [id ,sockets]) => /*console.log('IDDSSSSS', id, sockets) || */isBusinessAvailable || !!sockets.length,
+				false
+			)
+
 			socket.emit(
 				'isBusinessAvailable', 
-				Object.entries(sockets.businessSignedIn).reduce(
-					(isBusinessAvailable, [,sockets]) => isBusinessAvailable || !!sockets.length,
-					false
-				)
+				isBusinessAvailable,
 			)
 		})
 	}
 
  	io.on('connection', socket => {
-
- 		console.log('CONNECTION');
 
  		let role;
 
@@ -39,11 +48,15 @@ module.exports = (io, auth) => {
  		const roles = {
 			businessNotSignedIn: {
 				login: credentials => {
+
+					console.log('CREDIENTIAL', credentials);
+
 					if (!auth.isBusinessLogin(credentials)) {
-						socket.emit('login', {error: 'Not business login'});
+						socket.emit('login', 'Not business login');
 						return;
 					}
 
+					console.log('AAAA');
 					changeRole(roles.businessSignedIn, credentials);
 					socket.emit('login');
 				},
@@ -58,6 +71,7 @@ module.exports = (io, auth) => {
 						socket
 					];
 
+					broadcastEndClients();
 					broadcastIsBusinessAvailable();
 				},
 
@@ -73,28 +87,32 @@ module.exports = (io, auth) => {
 				},
 
 				disconnect: () => {
-					sockets.businessSignedIn = sockets.businessSignedIn.filter(
+					sockets.businessSignedIn[username] = sockets.businessSignedIn[username].filter(
 						businessSignedIn => businessSignedIn !== socket
 					);
+
 					broadcastIsBusinessAvailable();
 				},
 			},
 
 			endClientGuest: {
 				connect: () => {
+					console.log('THIS?');
 					endClientId = endClientSocketsAllocated++;
 					sockets.endClientGuest[endClientId] = socket;
+					broadcastEndClients();
+					broadcastIsBusinessAvailable();
+					console.log('END USER CONNECTED');
 				},
 
 				message: message => {
-					const receivers = sockets.businessSignedIn[message.to];
-					receivers.forEach(receiver => {
-						receiver.emit('message', {
+					forEachBusinessSocket(
+						socket => console.log('emiting message', message) || socket.emit('message', {
 							content: message.content,
 							at: moment(),
-							from: username,
-						});
-					});
+							from: endClientId,
+						})
+					);
 				},
 
 				disconnect: () => {
@@ -105,27 +123,27 @@ module.exports = (io, auth) => {
 
 		};
 
- 		const changeRole = (newRole, connectProps) => {
+ 		const changeRole = (newRole, connectParam) => {
 
  			const exitRole = () => {
 				Object.entries(role).forEach(([type, listener]) => {
 					socket.removeListener(type, listener);
 				});
-				role.disconnect();
+				if (role.disconnect) role.disconnect();
 				role = undefined;
 			};
 
-			const enterRole = newRole => {
+			const enterRole = () => {
 				Object.entries(newRole).forEach(([type, listener]) => {
 					socket.on(type, listener);
 				});
 				role = newRole;
-				newRole.connect(...connectProps);
+				if (newRole.connect) newRole.connect(connectParam);
 				
 			}
 
 			if (role) exitRole();
-			enterRole(newRole)
+			enterRole()
 		}
 
 		if (auth.isBusinessClient(socket)) {
